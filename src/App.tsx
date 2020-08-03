af28dcf341235b754595b7ad27e4bfb2107c8cce
Ricardo <ricardo.dalcinh@gmail.com>
2020-08-03 14:09:47 -0300

    Responsive landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './assets/styles/global.css'
+
+import Landing from './pages/Landing'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <Landing />
     </div>
   )
 }
