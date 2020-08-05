6e3aa2cc7240ee84b9b448fae3e606403d57a16c
Ricardo <ricardo.dalcinh@gmail.com>
2020-08-05 08:45:58 -0300

    Input, select and textarea components + styles

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -8,9 +8,14 @@ import './styles.css'
 
 interface PageHeaderProps {
   title: string
+  description?: string
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ children, title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  children,
+  title,
+  description,
+}) => {
   return (
     <header className='page-header'>
       <div className='top-bar-container'>
@@ -22,6 +27,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ children, title }) => {
 
       <div className='header-content'>
         <strong>{title}</strong>
+        {description && <p>{description}</p>}
         {children}
       </div>
     </header>
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,13 +1,61 @@
 import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Input'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
 
 function TeacherForm() {
   return (
     <div id='page-teacher-form' className='container'>
-      <PageHeader title='Que incrível que você quer dar aulas.' />
+      <PageHeader
+        title='Que incrível que você quer dar aulas.'
+        description='O primeiro passo é preencher esse formulário de inscrição'
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name='name' label='Nome completo' />
+          <Input name='avatar' label='Avatar' />
+          <Input name='whatsapp' label='Whatsapp' />
+          <Textarea name='bio' label='Biografia' />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Select
+            name='subject'
+            label='Matéria'
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Químimca', label: 'Químimca' },
+              { value: 'Português', label: 'Português' },
+            ]}
+          />
+          <Input name='cost' label='Custo da sua hora por aula' />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt='Aviso importante' />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type='button'>Salvar cadastro</button>
+        </footer>
+      </main>
     </div>
   )
 }
