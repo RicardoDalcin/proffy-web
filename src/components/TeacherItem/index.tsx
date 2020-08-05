0e980cb6779bd4df0f942eb4b3973ca77b6840a3
Ricardo <ricardo.dalcinh@gmail.com>
2020-08-05 10:05:28 -0300

    Search teachers in teacher list

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,32 +4,37 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number
+  name: string
+  avatar: string
+  whatsapp: string
+  bio: string
+  subject: string
+  cost: number
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className='teacher-item'>
       <header>
-        <img
-          src='https://avatars0.githubusercontent.com/u/58394781?s=460&u=e083b958ef3a7e128dedc5a94a4f040d8924296e&v=4'
-          alt='Ricardo Dalcin'
-        />
+        <img src={teacher.avatar} alt='Ricardo Dalcin' />
         <div>
-          <strong>Ricardo Dalcin</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
+
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
         <button type='button'>
           <img src={whatsappIcon} alt='Whatsapp' />
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
 import Input from '../../components/Input'
 import Select from '../../components/Select'
 
 import './styles.css'
+import api from '../../services/api'
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([])
+
+  const [subject, setSubject] = useState('')
+  const [weekDay, setWeekDay] = useState('')
+  const [time, setTime] = useState('')
+
+  const searchTeachers = async (e: FormEvent) => {
+    e.preventDefault()
+
+    const res = await api.get('/classes', {
+      params: { subject, week_day: weekDay, time },
+    })
+
+    setTeachers(res.data)
+  }
+
   return (
     <div id='page-teacher-list' className='container'>
       <PageHeader title='Estes são os proffys disponíveis.'>
-        <form id='search-teachers'>
+        <form id='search-teachers' onSubmit={searchTeachers}>
           <Select
             name='subject'
             label='Matéria'
@@ -25,6 +42,8 @@ function TeacherList() {
               { value: 'Químimca', label: 'Químimca' },
               { value: 'Português', label: 'Português' },
             ]}
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
           />
           <Select
             name='week_day'
@@ -38,15 +57,24 @@ function TeacherList() {
               { value: '5', label: 'Sexta-feira' },
               { value: '6', label: 'Sábado' },
             ]}
+            value={weekDay}
+            onChange={(e) => setWeekDay(e.target.value)}
           />
-          <Input name='time' label='Hora' type='time' />
+          <Input
+            name='time'
+            label='Hora'
+            type='time'
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+          <button type='submit'>Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => (
+          <TeacherItem key={teacher.id} teacher={teacher} />
+        ))}
       </main>
     </div>
   )
