644efe3be7f8a5860c8c99aae379c8422de46a41
Ricardo <ricardo.dalcinh@gmail.com>
2020-08-05 09:05:19 -0300

    Schedule items in teacher form

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PageHeader from '../../components/PageHeader'
 import Input from '../../components/Input'
@@ -10,6 +10,13 @@ import warningIcon from '../../assets/images/icons/warning.svg'
 import './styles.css'
 
 function TeacherForm() {
+  const initialScheduleItemState = { week_day: 0, from: '', to: '' }
+  const [scheduleItems, setScheduleItems] = useState([initialScheduleItemState])
+
+  const addNewScheduleItem = () => {
+    setScheduleItems([...scheduleItems, initialScheduleItemState])
+  }
+
   return (
     <div id='page-teacher-form' className='container'>
       <PageHeader
@@ -45,6 +52,35 @@ function TeacherForm() {
             ]}
           />
           <Input name='cost' label='Custo da sua hora por aula' />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis{' '}
+            <button type='button' onClick={addNewScheduleItem}>
+              + Novo horário
+            </button>
+          </legend>
+
+          {scheduleItems.map((scheduleItem) => (
+            <div className='schedule-item' key={scheduleItem.week_day}>
+              <Select
+                name='week_day'
+                label='Dia da semana'
+                options={[
+                  { value: '0', label: 'Domingo' },
+                  { value: '1', label: 'Segunda-feira' },
+                  { value: '2', label: 'Terça-feira' },
+                  { value: '3', label: 'Quarta-feira' },
+                  { value: '4', label: 'Quinta-feira' },
+                  { value: '5', label: 'Sexta-feira' },
+                  { value: '6', label: 'Sábado' },
+                ]}
+              />
+              <Input name='from' label='Das' type='time' />
+              <Input name='to' label='Até' type='time' />
+            </div>
+          ))}
         </fieldset>
 
         <footer>
